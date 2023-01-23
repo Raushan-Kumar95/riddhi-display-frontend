d8a63a8aad6043030c3e5e48a7d92c5cec39cc58
rajeevrk9901 <rajeevrk9901@gmail.com>
2023-01-23 15:48:59 +0530

    vite starter for riddhi

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1 className='text-3xl'>Hello RRR</h1>
+    </div>
+  )
+}
+
+export default App
