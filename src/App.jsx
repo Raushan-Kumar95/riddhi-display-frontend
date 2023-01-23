11098bca02a82c3d20b405bbef69a523d77d0855
rajeevrk9901 <rajeevrk9901@gmail.com>
2023-01-23 16:06:10 +0530

    postcss init using npm tailwindcss init -p

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className='text-3xl'>Hello RRR</h1>
+      <h1 className='text-3xl text-red-400'>Hello RRR</h1>
     </div>
   )
 }
