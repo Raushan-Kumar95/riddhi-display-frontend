c00918b5f05765de299b311635a4499a57ca438f
Raushan Kumar <raushankp75@gmail.com>
2023-01-25 15:38:24 +0530

    Made Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <div>
             {/* Header Top */}
-            <div className='bg-black text-white py-4 px-10 flex flex-row justify-between'>
+            <div className='bg-black text-white py-3 px-10 flex flex-row md:justify-between justify-center'>
 
                 <div className='hidden md:block'>
                     <ul className='flex flex-row text-2xl'>
@@ -16,10 +16,10 @@ const Navbar = () => {
                     </ul>
                 </div>
 
-                <div>
-                    <ul className='flex md:flex-row flex-col md:mx-5 mx-auto '>
-                        <li className='mx-3'><a href="tel:+91 9825072799"><i class="fa-solid fa-phone"></i> +91 9825072799</a></li>
-                        <li className='mx-3'><a href="mailto:info@riddhidisplay.com"><i class="fa-regular fa-envelope"></i> info@riddhidisplay.com</a></li>
+                <div className=''>
+                    <ul className='flex md:flex-row flex-col md:mx-5  '>
+                        <li className='mx-3 text-center'><a href="tel:+91 9825072799"><i class="fa-solid fa-phone"></i> +91 9825072799</a></li>
+                        <li className='mx-3 text-center'><a href="mailto:info@riddhidisplay.com"><i class="fa-regular fa-envelope"></i> info@riddhidisplay.com</a></li>
 
                     </ul>
 
@@ -28,20 +28,27 @@ const Navbar = () => {
 
 
             {/* Header Bottom */}
-            <nav className="flex shadow-xl justify-between">
-                <div className='py-7 px-14'>
+            <nav className="flex md:flex-row flex-col shadow-xl justify-between overflow-hidden md:py-2  md:px-14">
+                <div className='flex flex-row justify-between md:px-2 px-5 py-3'>
                     <img src="https://www.riddhidisplay.com/wp-content/uploads/2021/04/logo.png" alt="Logo" />
+
+                    <div className='py-4 text-3xl md:hidden block'>
+                        <i class="fa-solid fa-bars"></i>
+                    </div>
+
                 </div>
 
-                <ul className='md:flex md:flex-row flex-col md:text-center py-9 px-14 font-semibold text-lg'>
-                    {HeaderData.map(nav => {
-                        return (
-                            <li className='px-5'>
-                                <a href={nav.link}>{nav.name}</a>
-                            </li>
-                        )
-                    })}
-                </ul>
+                <div className='menu'>
+                    <ul className='md:flex md:flex-row flex-col md:text-center font-semibold text-lg text-center md:py-7 py-10'>
+                        {HeaderData.map(nav => {
+                            return (
+                                <li className='md:px-5 md:py-1 py-2'>
+                                    <a href={nav.link}>{nav.name}</a>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
             </nav>
         </div>
     )
