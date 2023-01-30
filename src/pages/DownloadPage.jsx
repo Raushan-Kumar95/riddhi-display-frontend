c556fc5fcb3805b4a4bb98c3a44b95a4b51d27ac
Raushan132 <raushankumar132abc@gmail.com>
2023-01-30 15:55:02 +0530

    fix break point...

--- src/pages/DownloadPage.jsx
+++ src/pages/DownloadPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import DownloadBrohure from '../components/download/DownloadBrohure'
+import DownloadBroucher from '../components/download/DownloadBroucher'
 import TopBannerDownload from '../components/download/TopBannerDownload'
 
 
@@ -9,7 +9,7 @@ const DownloadPage = () => {
   return (
     <div>
         <TopBannerDownload name="Download" />
-        <DownloadBrohure />
+        <DownloadBroucher />
     </div>
   )
 }
