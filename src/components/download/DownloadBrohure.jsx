ace7c65f127de91e65eb5fcf97ca91d7fe4aef87
Raushan132 <raushankumar132abc@gmail.com>
2023-01-30 12:45:49 +0530

    DownloadBrohure.jsx is created and complete design with responsive

--- src/components/download/DownloadBrohure.jsx
+++ src/components/download/DownloadBrohure.jsx
@@ -1,24 +1,66 @@
 import React from 'react'
 
 const DownloadBrohure = () => {
-  return (
-    <>
-        <div className="p-24 flex flex-col items-center ">
-            <div className="text-5xl font-bold  ">Fill Your Detail Below To Download Our Brochure</div>
-            <hr/>
-            <div className="flex flex-col">
-                <form>
-                    <div className="flex flex-col">
-                        <label htmlFor="">First Name*</label>
-                        <input type="text" className="" value="hello" />
-                    </div>
+    return (
+        <>
+            <div className="my-16 mx-8  flex flex-col md:items-center ">
+                <div className="text-3xl md:text-5xl md:w-[900px] font-bold  ">Fill Your Detail Below To Download Our Brochure</div>
+                  <hr className="w-1/3 md:w-1/6 mt-10 relative left-[50%] translate-x-[-50%] md:left-0 md:translate-x-[0] border-black "/>
+                <div className="my-10">
+                    <form  className="flex flex-col gap-5 md:w-[900px]">
+                        <div className="flex flex-col gap-2">
+                            <label htmlFor="" className="font-bold">First Name*</label>
+                            <input type="text" className="border-2 p-2" value=""  required />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <label htmlFor="" className="font-bold">Last Name*</label>
+                            <input type="text" className="border-2 p-2" value=""  required />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <label htmlFor="" className="font-bold">Company Name*</label>
+                            <input type="text" className="border-2 p-2" value=""  required />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <label htmlFor="" className="font-bold">Mobile Number*</label>
+                            <input type="text" className="border-2 p-2" value=""  required />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <label htmlFor="" className="font-bold ">Email Address*</label>
+                            <input type="text" className="border-2 p-2" value=""  required />
+                        </div>
+                        <div className="flex flex-col md:flex-row gap-2 md:gap-10">
+                            <div className="flex flex-col gap-2">
+                                <label htmlFor="" className="font-bold">Country*</label>
+                                <input type="text" className="border-2 p-2" value=""  required />
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <label htmlFor="" className="font-bold">State*</label>
+                                <input type="text" className="border-2 p-2" value=""  required />
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <label htmlFor="" className="font-bold">City*</label>
+                                <input type="text" className="border-2 p-2" value=""  required />
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <label htmlFor="" className="font-bold">Select Broucher*</label>
+                            <select className="border-2 p-2 bg-[url('https://www.riddhidisplay.com/wp-content/themes/riddhi-display/images/down-img.png')]  bg-no-repeat appearance-none bg-[center_right_1rem] bg-[length:20px_15px] ">
+                                    <option value="">Display Broucher</option>
+                                    <option value="">kitchen Broucher</option>
+                            </select>
+                        </div>
+                        <div className="flex flex-col">
 
-                </form>
+                            <input type="submit" className="border-2 md:w-1/3 p-2 bg-red-700 font-bold text-white cursor-pointer" value="Download Broucher" />
+                        </div>
+
+
+                    </form>
+                </div>
             </div>
-        </div>
 
-    </>
-  )
+        </>
+    )
 }
 
 export default DownloadBrohure
