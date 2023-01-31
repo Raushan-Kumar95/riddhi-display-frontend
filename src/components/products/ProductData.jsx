72d8fdc9196d1a72646aba3634e9fcc90f9c581d
Raushan132 <raushankumar132abc@gmail.com>
2023-01-31 13:40:15 +0530

    edit images

--- src/components/products/ProductData.jsx
+++ src/components/products/ProductData.jsx
@@ -4,49 +4,49 @@
 
 export const ColdDisplayData = [
     {
-        "img": "ProductImage/counter/Display_Counter.png",
+        "img": "/ProductImage/counter/Display_Counter.png",
         "name": "Display Counter",
         "alt": "Display Counter",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/counter/Ice_Cream_Parlor.png",
+        "img": "/ProductImage/counter/Ice_Cream_Parlor.png",
         "name": "Ice Cream Parlor",
         "alt": "Ice Cream Parlor",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/counter/Salad_Counter.png",
+        "img": "/ProductImage/counter/Salad_Counter.png",
         "name": "Salad Counter",
         "alt": "Salad Counter",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/counter/Salad_Counter_2.png",
+        "img": "/ProductImage/counter/Salad_Counter_2.png",
         "name": "Salad Counter ",
         "alt": "Salad Counter ",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/counter/Sweet_Display_Counter.png",
+        "img": "/ProductImage/counter/Sweet_Display_Counter.png",
         "name": "Sweet Display Counter",
         "alt": "Sweet Display Counter",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/counter/Sweet_Display_Counter_2.png",
+        "img": "/ProductImage/counter/Sweet_Display_Counter_2.png",
         "name": "Sweet Display Counter ",
         "alt": "Sweet Display Counter ",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/counter/Sweet_Display_Counter_23.png",
+        "img": "/ProductImage/counter/Sweet_Display_Counter_23.png",
         "name": "Sweet Display Counter ",
         "alt": "Sweet Display Counter ",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/counter/Sweet_Display_Counter_3.png",
+        "img": "/ProductImage/counter/Sweet_Display_Counter_3.png",
         "name": "Sweet Display Counter ",
         "alt": "Sweet Display Counter ",
         "size": "Customized"
@@ -59,43 +59,43 @@ export const ColdDisplayData = [
 export const CookingEquipmentData = [
     
         {
-            "img": "ProductImage/grill/Charcoal_Grill.png",
+            "img": "/ProductImage/grill/Charcoal_Grill.png",
             "name": "Charcoal Grill",
             "alt": "Charcoal Grill",
             "size": "Customized"
         },
         {
-            "img": "ProductImage/grill/Cooking_Vessels.png",
+            "img": "/ProductImage/grill/Cooking_Vessels.png",
             "name": "Cooking Vessels",
             "alt": "Cooking Vessels",
             "size": "Customized"
         },
         {
-            "img": "ProductImage/grill/Deep_Fat_Fryer.png",
+            "img": "/ProductImage/grill/Deep_Fat_Fryer.png",
             "name": "Deep Fat Fryer",
             "alt": "Deep Fat Fryer",
             "size": "Customized"
         },
         {
-            "img": "ProductImage/grill/Idli_Steamer.png",
+            "img": "/ProductImage/grill/Idli_Steamer.png",
             "name": "Idli Steamer",
             "alt": "Idli Steamer",
             "size": "Customized"
         },
         {
-            "img": "ProductImage/grill/Pasta_Cooker.png",
+            "img": "/ProductImage/grill/Pasta_Cooker.png",
             "name": "Pasta Cooker",
             "alt": "Pasta Cooker",
             "size": "Customized"
         },
         {
-            "img": "ProductImage/grill/Shawarma_Grill.png",
+            "img": "/ProductImage/grill/Shawarma_Grill.png",
             "name": "Shawarma Grill",
             "alt": "Shawarma Grill",
             "size": "Customized"
         },
         {
-            "img": "ProductImage/grill/SS_Mobile_Tandoor.png",
+            "img": "/ProductImage/grill/SS_Mobile_Tandoor.png",
             "name": "SS Mobile Tandoor",
             "alt": "SS Mobile Tandoor",
             "size": "Customized"
@@ -108,55 +108,55 @@ export const CookingEquipmentData = [
 export const CustomizedSolutionsData = [
  
     {
-        "img": "ProductImage/CustomizedSolutions/allThing.png",
+        "img": "/ProductImage/CustomizedSolutions/allThing.png",
         "name": "allThing",
         "alt": "allThing",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/CustomizedSolutions/CustomizedSolutions_03.png",
+        "img": "/ProductImage/CustomizedSolutions/CustomizedSolutions_03.png",
         "name": "CustomizedSolutions ",
         "alt": "CustomizedSolutions ",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/CustomizedSolutions/CustomizedSolutions_06.png",
+        "img": "/ProductImage/CustomizedSolutions/CustomizedSolutions_06.png",
         "name": "CustomizedSolutions ",
         "alt": "CustomizedSolutions ",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/CustomizedSolutions/CustomizedSolutions_09.png",
+        "img": "/ProductImage/CustomizedSolutions/CustomizedSolutions_09.png",
         "name": "CustomizedSolutions ",
         "alt": "CustomizedSolutions ",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/CustomizedSolutions/CustomizedSolutions_15.png",
+        "img": "/ProductImage/CustomizedSolutions/CustomizedSolutions_15.png",
         "name": "CustomizedSolutions ",
         "alt": "CustomizedSolutions ",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/CustomizedSolutions/CustomizedSolutions_18.png",
+        "img": "/ProductImage/CustomizedSolutions/CustomizedSolutions_18.png",
         "name": "CustomizedSolutions ",
         "alt": "CustomizedSolutions ",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/CustomizedSolutions/CustomizedSolutions_23.png",
+        "img": "/ProductImage/CustomizedSolutions/CustomizedSolutions_23.png",
         "name": "CustomizedSolutions ",
         "alt": "CustomizedSolutions ",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/CustomizedSolutions/CustomizedSolutions_24.png",
+        "img": "/ProductImage/CustomizedSolutions/CustomizedSolutions_24.png",
         "name": "CustomizedSolutions ",
         "alt": "CustomizedSolutions ",
         "size": "Customized"
     },
     {
-        "img": "ProductImage/CustomizedSolutions/CustomizedSolutions_25.png",
+        "img": "/ProductImage/CustomizedSolutions/CustomizedSolutions_25.png",
         "name": "CustomizedSolutions ",
         "alt": "CustomizedSolutions ",
         "size": "Customized"
