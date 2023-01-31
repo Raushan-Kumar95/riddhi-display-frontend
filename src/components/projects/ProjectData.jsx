e3a4211cbc10d8a49f6e422b49aa9830d3e2352a
rajeevrk9901 <rajeevrk9901@gmail.com>
2023-01-31 10:41:12 +0530

    added images prototype for product page

--- src/components/projects/ProjectData.jsx
+++ src/components/projects/ProjectData.jsx
@@ -3,28 +3,28 @@
 export const ProductsData = [
     // title = "COLD DISPLAY COUNTER",
     {
-        img: "public/Display_Counter/salad-display-counter.png",
+        img: "/Display_Counter/salad-display-counter.png",
         name:"Salad Counter",
         size:"Customized"
     },
     {
-        img: "public/Display_Counter/bakery-diplay-counter-vertical.png",
+        img: "/Display_Counter/bakery-diplay-counter-vertical.png",
         name:"Vertical Display Counter",
         size:"Customized"
     },
     {
-        img: "public/Display_Counter/Ice-Cream-Parlour-Counter-w300.jpg",
+        img: "/Display_Counter/Ice-Cream-Parlour-Counter-w300.jpg",
         name:"Ice Cream Parlour",
         size:"Customized"
     },
     {
-        img: "public/Display_Counter/bakery-display-counter-4-feet-500x500.webp",
+        img: "/Display_Counter/bakery-display-counter-4-feet-500x500.webp",
         name:"Bakery/Sweet Display Counter",
         size:"Customized"
     },
     
     {
-        img: "public/Display_Counter/sweets-display-counter-06-1345821.jpg",
+        img: "/Display_Counter/sweets-display-counter-06-1345821.jpg",
         name:"Bakery/Sweet Display Counter",
         size:"Customized"
     },
